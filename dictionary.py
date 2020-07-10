129ec966349f26017f4db3244814b4d3d3730fd0
Ramanathan Varadharajan <ramanathan.varadharajan@outlook.com>
2020-07-10 21:24:00 +0000

    set methods take lists as inputs

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,12 @@
-import pandas as pd
+#import pandas as pd
 dictionary = {
   'username':['Ram', 'Daphne', 'Ashan', 'Momota'],
   'rank':[4,2,3,1]
 }
 
 # Just trying out curious stuff!
-df=pd.DataFrame.from_dict(dictionary)
-print(df)
+#df=pd.DataFrame.from_dict(dictionary)
+#print(df)
 
 '''
 keys should be immutable in dictionary. (TypeError occurs unless)
