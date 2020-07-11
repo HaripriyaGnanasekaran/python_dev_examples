92d44e98c42345aa1da1d4a0e10ae69bc20ddcf1
Ramanathan Varadharajan <ramanathan.varadharajan@outlook.com>
2020-07-11 15:02:15 +0000

    some refinements

--- palindrome.py
+++ palindrome.py
@@ -1,16 +1,20 @@
 wordlist = [
-  'madam',
-  'madame'
+  'Never odd or even',
+  'dogma I am God',
+  "Madam Im Adam" ,
+  "Able was I ere I saw Elba",
+  'I am not a palindrome'
 ]
 
 
-for word in wordlist:
-  even = True 
+for rawword in wordlist:
   count = 0
+  word = rawword.replace(" ","")
+  word = word.lower()
   for letter in set(word):
     even = (word.count(letter))%2
     if even==True:
       count += 1
 
 
-  print(f'{word}: Cannot be a palindrome') if count >1 else print(f'{word}: Can be made into palindrome')
+  print(f'{rawword}: Cannot be a palindrome') if count >1 else print(f'{rawword}: Can be made into palindrome')
