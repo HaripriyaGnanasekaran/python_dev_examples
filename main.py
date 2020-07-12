fb452fb7bb0237ccba6a4fdc53b821c0a6c9a8f4
Ramanathan Varadharajan <ramanathan.varadharajan@outlook.com>
2020-07-12 13:47:58 +0000

    folder renamed

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-#import age # calculate the age
-#import password
-#import lists
-#import dictionary
-#import tuple
-#import sets
-#import shortcircuit
-#import palindrome
-#import trickycounter
-#import infty
-#import tree
-import advanced.functions as fn
+
+import section_4.functions as fn
 
 lister = [-89,-86,-4,-56,-124,-2]
 print(fn.highest_even(lister))
