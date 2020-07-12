8ba755e93b009d8a059599575c88ebf43f6e75d4
Ramanathan Varadharajan <ramanathan.varadharajan@outlook.com>
2020-07-12 09:54:28 +0000

    imshow explored

--- tree.py
+++ tree.py
@@ -1,14 +1,39 @@
-image = [
-  [0,0,0,1,0,0,0],
-  [0,0,1,1,1,0,0],
-  [0,1,1,1,1,1,0],
-  [1,1,1,1,1,1,1],
-  [0,0,0,1,0,0,0],
-  [0,0,0,1,0,0,0]
-]
+#image = [
+#  [0,0,0,1,0,0,0],
+#  [0,0,1,1,1,0,0],
+#  [0,1,1,1,1,1,0],
+#  [1,1,1,1,1,1,1],
+#  [0,0,0,1,0,0,0],
+#  [0,0,0,1,0,0,0]
+#]
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = Image.open('marirs.jpg').convert('L')
+plt.imshow(img,cmap='BuPu')
+plt.show()
+input('continue')
+
+image = np.array(img)
+image = ~image  # invert B&W
+image[image > 0] = 1
+
+
+
+print(image.shape)
+plt.imshow(image)
+plt.show()
+input('do we run?')
 
 for stacks in image:
   dots=[]
   for pixels in stacks:
     dots.append(" ") if pixels ==0 else dots.append("*")
   print(''.join(dots))
+
+print('       ')
+for stacks in image:
+  for pixels in stacks:
+    print(" ", end='') if pixels ==0 else print("*",end='')
+  print('')
